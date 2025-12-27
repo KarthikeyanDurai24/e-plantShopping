cdf30e9d28d1f62d30f0e0d52495912d6d92ea3b
KarthikeyanDurai24 <138101847+KarthikeyanDurai24@users.noreply.github.com>
2025-12-27 14:28:48 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Provider } from 'react-redux'; // Import Provider to enable Redux
+import store from './store.js'; // Import the global Redux store
+
+// Render the App component wrapped with Provider for Redux access
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
